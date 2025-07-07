d5a666da2ba9b5822e979934eadcc513abb0a389
Audrey Boucher <audrey@abtasty.com>
2025-07-07 08:43:28 +0200

    page: Contacts

--- src/pages/Contacts/Contacts.tsx
+++ src/pages/Contacts/Contacts.tsx
@@ -1,24 +1,77 @@
-import React, { FC } from 'react';
-import { Table } from '@radix-ui/themes';
+import React, { useState, type FC } from 'react';
 
-import { Nav } from '../../components';
+import { Layout, InfoDialog, EditDialog, DeleteDialog } from '../../components';
+
+import { emptyList, emptyContact, getContacts } from '../../api/methods/contacts';
+
+import { sendFakeRequest, Method } from '../../utils/api';
+import type {
+  Contact,
+  CreateContactRequest,
+  CreateContactResponse,
+  DeleteContactRequest,
+  DeleteContactResponse,
+  GetContactByIdRequest,
+  GetContactByIdResponse,
+  GetContactsRequest,
+  GetContactsResponse,
+  PaginationResponse,
+  UpdateContactRequest,
+  UpdateContactResponse,
+} from '../../types/api';
+import type { Content, Field } from '../../types/common';
+
+import FIELDS from './fields.json';
+
+const dialogs: Content<Contact> = {
+  [Method.GET]: {
+    title: "More details",
+    dialog: (props) => <InfoDialog {...props} />,
+    action: async ({ id }: Contact) =>
+      sendFakeRequest<GetContactByIdRequest, GetContactByIdResponse>(Method.GET, `/contacts/${id}`, null),
+  },
+  [Method.POST]: {
+    title: "Create new contact",
+    submitButton: "Add",
+    dialog: (props) => <EditDialog {...props} />,
+    action: async (contact: Contact) => 
+      sendFakeRequest<CreateContactRequest, CreateContactResponse>(Method.POST, `/contacts`, contact),
+    admin: true,
+  },
+  [Method.PATCH]: {
+    title: "Edit",
+    submitButton: "Update",
+    dialog: (props) => <EditDialog {...props} />,
+    action: async (contact: Contact) => 
+      sendFakeRequest<UpdateContactRequest, UpdateContactResponse>(Method.PATCH, `/contacts/${contact.id}`, contact),
+    admin: true,
+  },
+  [Method.DEL]: {
+    title: "Delete",
+    submitButton: "Confirm",
+    dialog: (props) => <DeleteDialog {...props} />,
+    action: async ({ id }: Contact) =>
+      sendFakeRequest<DeleteContactRequest, DeleteContactResponse>(Method.DEL, `/contacts/${id}`, null),
+    admin: true,
+  },
+};
 
 const Home: FC = () => {
-  return (
-    <>
-      <Nav />
+  const [contacts, setContacts] = useState<GetContactsResponse>(emptyList);
 
-      <Table.Root>
-        <Table.Header>
-          <Table.Row>
-            <Table.ColumnHeaderCell>First name</Table.ColumnHeaderCell>
-            <Table.ColumnHeaderCell>Last name</Table.ColumnHeaderCell>
-            <Table.ColumnHeaderCell>Email address</Table.ColumnHeaderCell>
-            <Table.ColumnHeaderCell>Phone number</Table.ColumnHeaderCell>
-          </Table.Row>
-        </Table.Header>
-      </Table.Root>
-    </>
+  const getItems = (filters: any) => {
+    getContacts(filters as GetContactsRequest).then(setContacts);
+  };
+
+  return (
+    <Layout
+      items={'items' in contacts ? contacts.items : []}
+      emptyItem={emptyContact}
+      getItems={getItems}
+      fields={FIELDS as Field<{}>[]}
+      pagination={contacts as PaginationResponse<Contact>}
+      dialogs={dialogs as any}
+    />
   );
 };
 

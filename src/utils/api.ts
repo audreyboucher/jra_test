b4defb637b10dd7cafffb55365f02d4feff345e5
Audrey Boucher <audrey@abtasty.com>
2025-07-07 08:35:12 +0200

    api: add new routes

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,22 +1,53 @@
 import { login } from '../api/methods/users';
+import { getContacts, getContactById, updateContactById, deleteContactById, createContact } from '../api/methods/contacts';
 
-import { LoginRequest } from '../types/api';
+import type {
+  LoginRequest,
+  GetContactsRequest,
+  UpdateContactRequest,
+  CreateContactRequest,
+} from '../types/api';
 
 export enum Method {
   GET = 'GET',
   POST = 'POST',
   PATCH = 'PATCH',
-  PUT = 'PUT',
   DEL = 'DELETE',
 };
 
-export const sendFakeRequest =  async <R, T>(method: Method, url: string, data?: R): Promise<T> => {
-  switch (url) {
-    case '/login':
-      return login(data as LoginRequest) as Promise<T>;
-    default:
-      return new Promise(() => {});
-  }
+export const sendFakeRequest =  async <T, R>(method: Method, url: string, data: T): Promise<R> => {
+  switch (method) {
+    case Method.GET:
+      switch (url) {
+        case '/contacts':
+          return getContacts(data as GetContactsRequest) as Promise<R>;
+        case url.match(/\/contacts\/(\w+)/)?.input:
+          return getContactById(url.match(/\/contacts\/(\w+)/)![1]) as Promise<R>;
+      };
+      break;
+    case Method.POST:
+      switch (url) {
+        case '/login':
+          return login(data as LoginRequest) as Promise<R>;
+        case '/contacts':
+          return createContact(data as CreateContactRequest) as Promise<R>;
+      };
+      break;
+    case Method.PATCH:
+      switch (url) {
+        case url.match(/\/contacts\/(\w+)/)?.input:
+          return updateContactById(url.match(/\/contacts\/(\w+)/)![1], data as UpdateContactRequest) as Promise<R>;
+      };
+      break;
+    case Method.DEL:
+      switch (url) {
+        case url.match(/\/contacts\/(\w+)/)?.input:
+          return deleteContactById(url.match(/\/contacts\/(\w+)/)![1]) as Promise<R>;
+      }
+      break;
+  };
+
+  return new Promise((_, reject) => { reject({ message: 'Request not found', }); }) as Promise<R>;
 };
 
 export const sendRequest = async <R, T>(method: Method, url: string, data?: R): Promise<T> => {
